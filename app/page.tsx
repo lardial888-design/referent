a3fb424318f970c507ec4f0dd9531915b1ccf6ce
lardial888-design <lardi.al888@gmail.com>
2025-12-22 07:34:19 +0300

    Я изучаю Next.js!

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       fontFamily: 'system-ui, sans-serif'
     }}>
       <h1 style={{ fontSize: '2.5rem', marginBottom: '1rem' }}>
-        Добро пожаловать в Next.js!
+        Я изучаю Next.js!
       </h1>
       <p style={{ fontSize: '1.2rem', color: '#666' }}>
         Я изучаю Next.js
